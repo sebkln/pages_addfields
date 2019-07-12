5399d19be76e925781bfc5cedfe2fa79727c06d4
Sebastian Klein <sebastian@sklein-medien.de>
2019-07-12 20:18:39 +0200

    [TASK] Update files according to current TYPO3 Coding Guidelines

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,40 +1,39 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
-}
+defined('TYPO3_MODE') or die();
 
 // Configure new fields:
-$fields = array(
-	'tx_pagesaddfields_customtext' => array(
-		'label' => 'LLL:EXT:pages_addfields/Resources/Private/Language/locallang_db.xlf:pages.tx_pagesaddfields_customtext',
-		'exclude' => 1,
-		'config' => array(
-			'type' => 'input',
-			'max' => 255
-		),
-	),
-	'tx_pagesaddfields_customcheckbox' => array(
-		'exclude' => 1,
-		'label' => 'LLL:EXT:pages_addfields/Resources/Private/Language/locallang_db.xlf:pages.tx_pagesaddfields_customcheckbox',
-		'config' => array(
-			'type' => 'check',
-			'default' => 0
-		)
-	)
-);
+$fields = [
+    'tx_pagesaddfields_customtext' => [
+        'label' => 'LLL:EXT:pages_addfields/Resources/Private/Language/locallang_db.xlf:pages.tx_pagesaddfields_customtext',
+        'exclude' => 1,
+        'config' => [
+            'type' => 'input',
+            'max' => 255
+        ],
+    ],
+    'tx_pagesaddfields_customcheckbox' => [
+        'exclude' => 1,
+        'label' => 'LLL:EXT:pages_addfields/Resources/Private/Language/locallang_db.xlf:pages.tx_pagesaddfields_customcheckbox',
+        'config' => [
+            'type' => 'check',
+            'default' => 0
+        ]
+    ]
+];
 
 // Add new fields to pages:
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $fields);
 
 // Make fields visible in the TCEforms:
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
-	'pages', // Table name
-	'--palette--;LLL:EXT:pages_addfields/Resources/Private/Language/locallang_db.xlf:pages.palette_title;tx_pagesaddfields', // Field list to add
-	'1', // List of specific types to add the field list to. (If empty, all type entries are affected)
-	'after:nav_title' // Insert fields before (default) or after one, or replace a field
+    'pages', // Table name
+    '--palette--;LLL:EXT:pages_addfields/Resources/Private/Language/locallang_db.xlf:pages.palette_title;tx_pagesaddfields',
+    // Field list to add
+    '1', // List of specific types to add the field list to. (If empty, all type entries are affected)
+    'after:nav_title' // Insert fields before (default) or after one, or replace a field
 );
 
 // Add the new palette:
-$GLOBALS['TCA']['pages']['palettes']['tx_pagesaddfields'] = array(
-	'showitem' => 'tx_pagesaddfields_customcheckbox,tx_pagesaddfields_customtext'
-);
+$GLOBALS['TCA']['pages']['palettes']['tx_pagesaddfields'] = [
+    'showitem' => 'tx_pagesaddfields_customcheckbox,tx_pagesaddfields_customtext'
+];
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,5 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
-}
+defined('TYPO3_MODE') or die();
 
 // Add new fields to the list of fields which should be overlaid on page records:
 $TYPO3_CONF_VARS['FE']['pageOverlayFields'] .= ',tx_pagesaddfields_customtext';
