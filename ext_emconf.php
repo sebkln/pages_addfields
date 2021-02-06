9bf933edb5985b63935c6db987f7cf2ec57223af
Sebastian Klein <sebastian@sklein-medien.de>
2021-02-06 13:46:48 +0100

    [TASK] Support composer installations

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Additional pages field(s)',
-    'description' => 'Provides a basic example how to extend pages with your own fields.',
+    'description' => 'Provides a basic example how to extend TYPO3 pages with your own fields.',
     'category' => 'example',
     'author' => 'Sebastian Klein',
     'author_email' => 'sebastian@sebkln.de',
